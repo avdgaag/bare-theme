98e6f479ca7183fc60e35c3c87e165c1ee4286b9
avdgaag <info@agwebdesign.nl>
2010-12-06 13:25:06 +0100

    Clean up wp_head

--- functions.php
+++ functions.php
@@ -10,4 +10,17 @@
  * @subpackage BareTheme
  * @since BareTheme 1.0
  */
+
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'recent_comments_style');
+
+function theme_remove_recent_comments_style() {
+    global $wp_widget_factory;
+    remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+}
+add_action('widgets_init', 'theme_remove_recent_comments_style');
+
 ?>
