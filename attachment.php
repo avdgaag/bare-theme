982e0ee0541927c28eb8c2dda04d8d7f2cf585f8
avdgaag <info@agwebdesign.nl>
2010-12-06 16:06:39 +0100

    Load single content on single pages

--- attachment.php
+++ attachment.php
@@ -9,5 +9,14 @@
 get_header();
 ?>
 
+<?php if(have_posts()): the_post(); ?>
+
+    <h1><?php the_title() ?></h1>
+
+    <?php the_content(); ?>
+
+<?php endif; ?>
+
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -14,5 +14,13 @@
 get_header();
 ?>
 
+<?php if(have_posts()): the_post(); ?>
+
+    <h1><?php the_title() ?></h1>
+
+    <?php the_content(); ?>
+
+<?php endif; ?>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
